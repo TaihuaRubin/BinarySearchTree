a558a4045f73974c8119f79723307cec5d2ea1af
Gabriel Lebec <glebec@gmail.com>
2018-07-23 18:04:10 -0400

    feat(bst): add simpler solutions (#1)

--- src/bst.js
+++ src/bst.js
@@ -6,6 +6,25 @@ class BinarySearchTree {
     this.magnitude = 1
   }
 
+  // // "simple" approach
+  // insert (val) {
+  //   this.magnitude++
+  //   if (val < this.value) { // left case
+  //     if (this.left) {
+  //       this.left.insert(val)
+  //     } else {
+  //       this.left = new BinarySearchTree(val)
+  //     }
+  //   } else { // right case
+  //     if (this.right) {
+  //       this.right.insert(val)
+  //     } else {
+  //       this.right = new BinarySearchTree(val)
+  //     }
+  //   }
+  // }
+
+  // concise / review video approach
   insert (val) {
     const direction = val < this.value ? 'left' : 'right'
     if (this[direction]) this[direction].insert(val)
@@ -13,6 +32,25 @@ class BinarySearchTree {
     this.magnitude++
   }
 
+  // // simple approach
+  // contains (val) {
+  //   if (this.value === val) return true
+  //   if (val < this.value) { // left case
+  //     if (this.left) {
+  //       return this.left.contains(val)
+  //     } else {
+  //       return false
+  //     }
+  //   } else { // right case
+  //     if (this.right) {
+  //       return this.right.contains(val)
+  //     } else {
+  //       return false
+  //     }
+  //   }
+  // }
+
+  // concise / review video approach
   contains (val) {
     if (this.value === val) return true
     const direction = val < this.value ? 'left' : 'right'
@@ -20,6 +58,25 @@ class BinarySearchTree {
     else return false
   }
 
+  // // simple approach
+  // depthFirstForEach (fn, opt) {
+  //   if (!opt) opt = 'in-order'
+  //   if (opt === 'pre-order') {
+  //     fn(this.value)
+  //     if (this.left) this.left.depthFirstForEach(fn, opt)
+  //     if (this.right) this.right.depthFirstForEach(fn, opt)
+  //   } else if (opt === 'in-order') {
+  //     if (this.left) this.left.depthFirstForEach(fn, opt)
+  //     fn(this.value)
+  //     if (this.right) this.right.depthFirstForEach(fn, opt)
+  //   } else if (opt === 'post-order') {
+  //     if (this.left) this.left.depthFirstForEach(fn, opt)
+  //     if (this.right) this.right.depthFirstForEach(fn, opt)
+  //     fn(this.value)
+  //   }
+  // }
+
+  // concise / review video approach
   // es6 Default Parameter - initializing with default values if no value passed in
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Default_parameters
   depthFirstForEach (fn, opt = 'in-order') {
